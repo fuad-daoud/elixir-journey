941712496311190cf5a347a9d1b54371ec10534e
Fuad Daoud <fuad.daoud@icloud.com>
2025-02-12 23:29:55 +0300

    refactor: community solution better overall

--- boutique-inventory/lib/boutique_inventory.ex
+++ boutique-inventory/lib/boutique_inventory.ex
@@ -2,37 +2,35 @@ defmodule BoutiqueInventory do
   def sort_by_price(inventory) do
     Enum.sort_by(
       inventory,
-      fn a -> a[:price] end,
+      & &1.price,
       :asc
     )
   end
 
   def with_missing_price(inventory) do
-    Enum.filter(inventory, fn i -> i[:price] == nil end)
+    Enum.filter(inventory, &(!&1.price))
   end
 
   def update_names(inventory, old_word, new_word) do
-    Enum.map(inventory, fn i ->
-      if String.match?(i[:name], ~r/#{old_word}/) do
-        %{i | name: String.replace(i[:name], ~r/#{old_word}/, new_word)}
-      else
-        i
-      end
+    Enum.map(inventory, fn item ->
+      %{item | name: String.replace(item.name, old_word, new_word)}
     end)
   end
 
   def increase_quantity(item, count) do
-    q = item.quantity_by_size
-
     %{
       item
-      | quantity_by_size: Enum.into(q, %{}, fn {k, v} -> {k, v + count} end)
+      | quantity_by_size:
+          item.quantity_by_size
+          |> Enum.into(%{}, fn {k, v} -> {k, v + count} end)
     }
   end
 
   def total_quantity(item) when item.quantity_by_size == %{}, do: 0
 
-  def total_quantity(item) do
-    Enum.reduce(Map.values(item.quantity_by_size), fn x, acc -> x + acc end)
+  def total_quantity(%{quantity_by_size: sizes}) do
+    sizes
+    |> Map.values()
+    |> Enum.reduce(fn x, acc -> x + acc end)
   end
 end
