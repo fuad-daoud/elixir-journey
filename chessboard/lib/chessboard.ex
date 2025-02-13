127b548e4072e83dbc6711410b6da6bc53a2da95
Fuad Daoud <fuad.daoud@icloud.com>
2025-02-13 19:53:01 +0300

    feat: solved chessboard

---
+++ chessboard/lib/chessboard.ex
@@ -0,0 +1,19 @@
+defmodule Chessboard do
+  def rank_range do
+    1..8
+  end
+
+  def file_range do
+    ?A..?H
+  end
+
+  def ranks do
+    rank_range()
+    |> Enum.map(fn x -> x end)
+  end
+
+  def files do
+    file_range()
+    |> Enum.map(fn x -> <<x::utf8>> end)
+  end
+end
