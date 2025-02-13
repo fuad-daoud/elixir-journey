976db9cb9d8da8d28fe63cf93ec496cd60c00bcf
Fuad Daoud <fuad.daoud@icloud.com>
2025-02-14 00:34:28 +0300

    refactor: looking community solutions

--- community-garden/lib/community_garden.ex
+++ community-garden/lib/community_garden.ex
@@ -5,37 +5,35 @@ defmodule Plot do
 end
 
 defmodule CommunityGarden do
-  use Agent
-
   def start(opts \\ []) do
-    Agent.start_link(fn -> %{count: 0, plots: []} end, opts)
+    Agent.start_link(fn -> {0, []} end, opts)
   end
 
   def list_registrations(pid) do
-    Agent.get(pid, fn state -> state.plots end)
+    Agent.get(pid, fn {_, plots} -> plots end)
   end
 
   def register(pid, register_to) do
-    Agent.get_and_update(pid, fn state ->
-      plot = %Plot{plot_id: state.count + 1, registered_to: register_to}
-      {plot, %{count: state.count + 1, plots: [plot | state.plots]}}
+    Agent.get_and_update(pid, fn {count, plots} ->
+      plot = %Plot{plot_id: count + 1, registered_to: register_to}
+      {plot, {count + 1, [plot | plots]}}
     end)
   end
 
   def release(pid, plot_id) do
-    Agent.update(pid, fn state ->
-      plots =
-        Enum.filter(state.plots, fn plot ->
+    Agent.update(pid, fn {count, plots} ->
+      new_plots =
+        Enum.filter(plots, fn plot ->
           plot.plot_id != plot_id
         end)
 
-      %{count: state.count, plots: plots}
+      {count, new_plots}
     end)
   end
 
   def get_registration(pid, plot_id) do
-    Agent.get(pid, fn state ->
-      state.plots
+    Agent.get(pid, fn {_, plots} ->
+      plots
       |> Enum.filter(fn plot -> plot.plot_id == plot_id end)
       |> List.first({:not_found, "plot is unregistered"})
     end)
