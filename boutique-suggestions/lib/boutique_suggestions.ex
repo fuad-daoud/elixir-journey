efc378090f1d98fb6a84d03489c06adcef59ace6
Fuad Daoud <fuad.daoud@icloud.com>
2025-02-13 23:22:03 +0300

    feat: solved boutique suggestions

---
+++ boutique-suggestions/lib/boutique_suggestions.ex
@@ -0,0 +1,11 @@
+defmodule BoutiqueSuggestions do
+  def get_combinations(tops, bottoms, options \\ []) do
+    max = Keyword.get(options, :maximum_price, 100)
+
+    for top <- tops,
+        bottom <- bottoms,
+        top.base_color != bottom.base_color and top.price + bottom.price < max do
+      {top, bottom}
+    end
+  end
+end
